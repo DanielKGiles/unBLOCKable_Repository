6ff3329a36e3a5edfb45e4b20748291a8ff04da4
DanielKGiles <dgiles4@lion.lmu.edu>
2021-02-09 01:02:28 -0800

    Basic Player controller
    The player now moves forward on a flat plane at a fixed speed with the camera following it. The PlayerController script is connected properly to the player.

---
+++ unBLOCKable/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody rb;
+    private float movementX;
+    private Vector3 movement;
+
+    private Vector3 playerTransformPosition;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started the game");
+
+        rb = GetComponent<Rigidbody>();
+
+        Vector3 playerTransformPosition = transform.position;
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Debug.Log("OnMove");
+
+        //Vector2 movementVector = movementValue.Get<Vector2>();
+        
+        //movement = new Vector3(movementVector.x, 0.0f, 0.0f);
+
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Debug.Log("FixedUpdate Called");
+        //rb.AddForce(movement * speed);
+        transform.position += new Vector3(0.5f, 0, 0);
+
+
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
