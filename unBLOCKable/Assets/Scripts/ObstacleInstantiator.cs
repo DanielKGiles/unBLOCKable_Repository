f3c3b280da7c9259b13c8bd131ce1a8717c5fc50
DanielKGiles <dgiles4@lion.lmu.edu>
2021-02-27 23:23:12 -0800

    Added double Blocks and cherries
    Blocks can now be two units tall and cherries can be instantiated.

--- unBLOCKable/Assets/Scripts/ObstacleInstantiator.cs
+++ unBLOCKable/Assets/Scripts/ObstacleInstantiator.cs
@@ -6,6 +6,7 @@ public class ObstacleInstantiator : MonoBehaviour
 {
     public GameObject player;
     public GameObject StandardBlock;
+    public GameObject CherryPickup;
 
     private int blockLocationCounter = 210;
     private bool blockPlaced = false;
@@ -13,11 +14,21 @@ public class ObstacleInstantiator : MonoBehaviour
     private int distanceToFinishMakingInitialBlocks = 200;
     private int distanceToStartMakingInitialBlocks = 20;
     private int blockIncrementationDistance = 10;
+    private int probabilityToProduceDoubleStoryBlock;
+
+    private int cherryLocationCounter = 210;
+    private bool cherryPlaced = false;
+    int[] cherryIndex = new int[] { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
+    private int distanceToFinishMakingInitialCherries = 200;
+    private int distanceToStartMakingInitialCherries = 20;
+    private int cherryIncrementationDistance = 10;
+
 
     // Start is called before the first frame update
     void Start()
     {
         blockLocationCounter = (int)player.transform.position[0] + distanceToFinishMakingInitialBlocks;
+        cherryLocationCounter = (int)player.transform.position[0] + distanceToFinishMakingInitialBlocks;
         InstantiateInitialObstacles();
     }
 
@@ -62,13 +73,68 @@ public class ObstacleInstantiator : MonoBehaviour
 
             Instantiate(StandardBlock, instantiationLocation, Quaternion.identity);
             blockPlaced = true;
+
+            float placeAdditionalBlockOnTop = Random.Range(0, 100);
+            if (placeAdditionalBlockOnTop > probabilityToProduceDoubleStoryBlock)
+            {
+                Instantiate(StandardBlock, new Vector3(instantiationLocation[0], 2, instantiationLocation[2]), Quaternion.identity);
+            }
         }        
 
     }
+    public void InstantiateInitialCherries()
+    {
+
+        int instantiateLocationX = (int)player.transform.position[0] + distanceToStartMakingInitialCherries;
+
+        for (int blockSegmentLocation = instantiateLocationX; blockSegmentLocation < distanceToFinishMakingInitialCherries; blockSegmentLocation += cherryIncrementationDistance)
+        {
+            int rowNumber = Random.Range(blockSegmentLocation, blockSegmentLocation + cherryIncrementationDistance);
+
+            //for (int numberOfblocks)
+            int trackSliceNumber = Random.Range(-2, 3);
+
+            Vector3 instantiationLocation = new Vector3(rowNumber, 1, trackSliceNumber);
+
+            Instantiate(CherryPickup, instantiationLocation, Quaternion.identity);
+
+        }
+
+    }
+    public void InstantiateContinuousCherries()
+    {
+        
+
+        //int instantiateLocationX = (int)player.transform.position[0] + blockCounter;
+        if (cherryLocationCounter + cherryIncrementationDistance < player.transform.position[0] + distanceToFinishMakingInitialCherries)
+        {
+            cherryLocationCounter += cherryIncrementationDistance;
+            cherryPlaced = false;
+        }
+        if (cherryPlaced != true)
+        {
+            int rowNumber = Random.Range(cherryLocationCounter, cherryLocationCounter + cherryIncrementationDistance);
+
+            //for (int numberOfblocks)
+            int trackSliceNumber = Random.Range(-2, 3);
+
+            Vector3 instantiationLocation = new Vector3(rowNumber, 1, trackSliceNumber);
+
+            Instantiate(CherryPickup, new Vector3(instantiationLocation[0], 2, instantiationLocation[2]), Quaternion.identity);
+            cherryPlaced = true;
+        }
+
+    }
+
+    //public void RemoveUnusedStandardBlocks
+    //{
+    //    if 
+    //}
 
     private void FixedUpdate()
     {
         InstantiateContinuousObstacles();
+        InstantiateContinuousCherries();
     }
 
     // Update is called once per frame
