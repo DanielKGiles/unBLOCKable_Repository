8a6c384112226876fa18c579f339a5d1fd267022
DanielKGiles <dgiles4@lion.lmu.edu>
2021-02-27 16:42:30 -0800

    Added lerped movement
    The movement is now much smoother using lerp. However, it is still a bit buggy in that it does away with lerping and just moves the object when the move button is pushed consecutively.

--- unBLOCKable/Assets/Scripts/PlayerController.cs
+++ unBLOCKable/Assets/Scripts/PlayerController.cs
@@ -10,13 +10,27 @@ public class PlayerController : MonoBehaviour
     public Vector3 forwardSpeedV3 = new Vector3(0.5f, 0, 0);
     public Vector3 moveLeftDistanceV3 = new Vector3(0, 0, 1.00f);
     public Vector3 moveRightDistanceV3 = new Vector3(0, 0, -1.00f);
-    public Vector3 moveUpDistanceV3 = new Vector3(0, 1.00f, 0);
-    public Vector3 moveDownDistanceV3 = new Vector3(0, -1.00f, 0);
+    //public Vector3 moveUpDistanceV3 = new Vector3(0, 1.00f, 0);
+    //public Vector3 moveDownDistanceV3 = new Vector3(0, -1.00f, 0);
     public Vector3 moveForwardDistanceV3 = new Vector3(1.00f, 0, 0);
+
+    public float jumpPower = 1.00f;
 
     public GameManager GameManager;
 
     public GameObject[] StandardBlocks;
+
+    private Vector3 leftRightVelocity = Vector3.zero;
+
+    public Vector3 positionToMoveTo = new Vector3(1000.00f, 1000.00f, 1000.00f);
+
+    private bool currentlyLerping = false;
+
+    public float lerpDuration = 0.01f;
+
+    public GameObject TestCube;
+
+    Vector3 targetPosition = new Vector3(4.00f,1.00f,0.00f);
 
 
     // Start is called before the first frame update
@@ -27,6 +41,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         Vector3 playerTransformPosition = transform.position;
+        Vector3 targetPosition = new Vector3(4.00f,1.00f,0.00f);
     }
     void OnMove(InputValue movementValue)
     {
@@ -38,37 +53,76 @@ public class PlayerController : MonoBehaviour
     {
         //Debug.Log("FixedUpdate Called");
         //rb.AddForce(movement * speed);
+
+
         transform.position += forwardSpeedV3;
 
 
 
 
-
     }
+
+    IEnumerator LerpPosition(float duration)
+    {
+        currentlyLerping = true;
+
+        float time = 0;
+        //Vector3 startPosition = transform.position;
+        Debug.Log(targetPosition);
+
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position[0], transform.position[1], targetPosition[2]), time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = new Vector3(transform.position[0], transform.position[1], targetPosition[2]);
+        currentlyLerping = false;
+    }
+
 
     private void OnMoveLeft() 
     {
-        transform.position += new Vector3(0, 0, 1.00f);
+        //transform.position += new Vector3(0, 0, 1.00f);
+        targetPosition = targetPosition + new Vector3(0, 0, 1.00f);
+        if (currentlyLerping == false) 
+        {
+        
+        StartCoroutine(LerpPosition(lerpDuration));
+
+        }
         Debug.Log("OnMoveLeft UPDATED");
     }
 
     private void OnMoveRight()
     {
-        transform.position += moveRightDistanceV3;
+        //transform.position += moveRightDistanceV3;
+        Debug.Log(targetPosition);
+        targetPosition = targetPosition + moveRightDistanceV3;
+        if (currentlyLerping == false)
+        {
+
+            StartCoroutine(LerpPosition(lerpDuration));
+
+        }
+
         //transform.position += Vector3.Lerp(transform.position, moveRightDistanceV3, 0.7f);
         Debug.Log("OnMoveRight");
+        
 
     }
 
     private void OnMoveUp()
     {
-        transform.position += moveUpDistanceV3;
+        //transform.position += moveUpDistanceV3;
+        rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
 
     }
 
     private void OnMoveDown()
     {
-        transform.position += moveDownDistanceV3;
+        //transform.position += moveDownDistanceV3;
+        rb.AddForce(Vector3.down * jumpPower, ForceMode.Impulse);
 
     }
 
@@ -89,7 +143,10 @@ public class PlayerController : MonoBehaviour
         Debug.Log("TriggerActivated");
         if (objectCollider.gameObject.CompareTag("StandardBlockCollisionTriggerCollider"))
         {
-            GameManager.DisplayCollisionParticleEffect(objectCollider.gameObject.transform, this.gameObject);
+            GameManager.DisplayCollisionParticleEffect(transform, this.gameObject);
+        } else if (objectCollider.gameObject.CompareTag("StandardBlockCollisionTriggerCollider"))
+        {
+            GameManager.DisplayCollisionParticleEffect(transform, this.gameObject);
         }
     }
 }
